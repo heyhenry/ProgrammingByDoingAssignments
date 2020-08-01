7b8b2c41b589e196e07361fd5fef6c80477cba0e
Nampa <henryyy3@hotmail.com>
2020-08-01 13:40:41 +0800

    Created a quiz program that provides multiple choices.

---
+++ src/ALittleQuiz/Quiz.java
@@ -0,0 +1,80 @@
+package ALittleQuiz;
+
+import java.util.Scanner;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String choice = "";
+
+        System.out.print("Are you ready? ");
+        String isReady = sc.nextLine();
+
+        if(isReady.equalsIgnoreCase("Yes")) {
+
+            System.out.println("Q1. What is the capital of Western Australia? ");
+            System.out.println("[A] Melbourne");
+            System.out.println("[B] Perth");
+            System.out.println("[C] Adelaide");
+            choice = sc.nextLine();
+
+            if(choice.equalsIgnoreCase("B")) {
+
+                System.out.println("You got it right!");
+
+            } else {
+
+                System.out.println("Oops, " + choice.toUpperCase() + " wasn't the right choice.");
+
+            }
+
+            System.out.println();
+            System.out.println("Q2. What is the currency called in Armenia? ");
+            System.out.println("[A] Rouble");
+            System.out.println("[B] Yen");
+            System.out.println("[C] Dram");
+            choice = sc.nextLine();
+
+            if(choice.equalsIgnoreCase("C")) {
+
+                System.out.println("You got it right!");
+
+            } else {
+
+                System.out.println("Oops, " + choice.toUpperCase() + " wasn't the right choice.");
+
+            }
+
+            System.out.println();
+            System.out.println("Q3. Who is the author of Harry Potter?");
+            System.out.println("[A] J. K. Rowling");
+            System.out.println("[B] Neil Gaiman");
+            System.out.println("[C] N. K. Jemisin");
+            choice = sc.nextLine();
+
+            if(choice.equalsIgnoreCase("A")) {
+
+                System.out.println("You got it right!");
+
+            } else {
+
+                System.out.println("Oops, " + choice.toUpperCase() + " wasn't the right choice.");
+
+            }
+
+        } else if (isReady.equalsIgnoreCase("No")) {
+
+            System.out.println("Oh okay, maybe another time.");
+
+        } else {
+
+            System.out.println("Looks like your answer strayed off topic! Please give a yes or no.");
+
+        }
+
+    }
+
+}
